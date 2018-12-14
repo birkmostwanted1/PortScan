0a8a073e6c084c278566a34f9bd954d9084c9dcd
Unknown <daniel111999@gmail.com>
2018-12-14 14:33:04 +0000

    wrote a port scanner

---
+++ PortScanner/PortScanner/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace PortScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var openPorts = new List<int>();
+
+            Parallel.For(1, 656, index =>
+            {
+                var port = Scan((index * 100) - 99, index * 100);
+                if (port.HasValue)
+                {
+                    openPorts.Add(port.Value);
+                }
+            });
+
+            foreach(var port in openPorts)
+            {
+                Console.WriteLine($"TCP Port {port} is open");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int? Scan(int portStart, int portRange)
+        {
+            var ip = "10.181.75.15";
+
+            var openPorts = new List<int>();
+
+            while (portStart <= portRange)
+            {
+                try
+                {
+                    var client = new TcpClient(ip, portStart);
+
+                    openPorts.Add(portStart);
+
+                    Console.WriteLine($"TCP Port {portStart} is open - on ip {ip}");
+
+                    return portStart;
+                }
+                catch (Exception)
+                {
+                    //Console.WriteLine($"Port {portStart} is closed - on ip {ip}");
+                }
+
+                portStart++;
+            }
+
+            return null;
+        }
+    }    
+}
---
+++ PortScanner/PortScanner/Scanner/PortScanner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace PortScanner.Scanner
+{
+    //public class Scanner 
+    //{
+    //    public void Scan()
+    //    {
+    //        var port = 1;
+    //        var ip = "127.0.0.1";
+
+    //        while(port < 5)
+    //        {
+    //            try
+    //            {
+    //                var client = new TcpClient(ip, port);
+
+    //                Console.WriteLine($"TCP Port {port} is open - on ip {ip}");
+
+    //            }
+    //            catch (Exception)
+    //            {
+    //                Console.WriteLine($"Failed to open TCP client on port {port} - ip {ip}");
+    //                throw;
+    //            }
+
+    //            port++;
+    //        }
+    //    }
+    //}
+}
